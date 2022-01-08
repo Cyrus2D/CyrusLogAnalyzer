cb6a5a57fe20331daeb24f0e88f8a6eeac814287
naderzare <nader.zare88@gmail.com>
2022-01-08 02:37:18 -0400

    add Math2d

---
+++ BaseCode/Math2D/geom_2d.py
@@ -0,0 +1,11 @@
+from BaseCode.Math2D.angle_deg import *
+from BaseCode.Math2D.vector_2d import *
+from BaseCode.Math2D.line_2d import *
+from BaseCode.Math2D.ray_2d import *
+from BaseCode.Math2D.sector_2d import *
+from BaseCode.Math2D.triangle_2d import Triangle2D
+from BaseCode.Math2D.segment_2d import Segment2D
+from BaseCode.Math2D.matrix_2d import *  # not necessary but optimized for 2d
+from BaseCode.Math2D.circle_2d import *
+from BaseCode.Math2D.rect_2d import *
+from BaseCode.Math2D.polygon_2d import *
