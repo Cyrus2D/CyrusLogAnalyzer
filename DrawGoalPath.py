f22ee334d08889e9122be8320c41a2561aebdc80
naderzare <nader.zare88@gmail.com>
2022-01-08 21:03:17 -0400

    update DrawGoalPath

--- DrawGoalPath.py
+++ DrawGoalPath.py
@@ -15,28 +15,31 @@ def main(path, side):
         #     break
         file_path = os.path.join(path, file_name)
         g = Game.read_log(file_path)
-        for ic in range(len(g.cycles) - 1, 0, -1):
-            goal_side = g.cycles[ic].is_before_goal
-            if g.cycles[ic].game_mode == GameMode.play_on and goal_side in side:
-                goal_sequence = []
-                for jc in range(ic, max(ic - 50, 0), -1):
-                    goal_sequence.append(g.cycles[jc].ball.pos)
-                goals_sequence.append(goal_sequence)
+        g.analyse_goal()
+        goal_cycles = g.left_goal_cycles if side == 'l' else g.right_goal_cycles
+        for c in goal_cycles:
+            goal_sequence = []
+            for jc in range(c - 1, max(c - 50, 0), -1):
+                cycle_object = g.get_cycle(jc, 0)
+                if cycle_object.game_mode != GameMode.play_on:
+                    break
+                goal_sequence.append(cycle_object.ball.pos())
+            goals_sequence.append(goal_sequence)
 
     plt.xlim(-55, +55)
     plt.ylim(-34, +34)
     for seq in goals_sequence:
-        plt.scatter([x.x for x in seq], [x.y for x in seq], s=1)
+        plt.scatter([x.x() for x in seq], [x.y() for x in seq], s=1)
     plt.show()
 
 
 if __name__ == '__main__':
     print('Usage: python3 DrawGoalPath RCGFilesPath r or l')
     path = 'Data'
-    side = ['r', 'l']
+    side = 'l'
     if len(sys.argv) > 1:
         path = sys.argv[1]
         if len(sys.argv) > 2:
-            side = [sys.argv[2]]
+            side = sys.argv[2]
 
     main(path, side)
